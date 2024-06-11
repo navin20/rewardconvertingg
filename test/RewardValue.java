c782c4cd1aa6327ee883d17fc6a3b03516182e08
navin20 <45535518+navin20@users.noreply.github.com>
2024-06-11 16:57:15 +1000

    Create RewardValue.java

---
+++ test/RewardValue.java
@@ -0,0 +1,28 @@
+package test;
+
+public class RewardValue {
+
+	private double cashValue;
+	private double milesValue;
+//	private double rate;// 0.0035
+//	
+	
+	
+	RewardValue(double cashValue){
+		this.cashValue = cashValue;
+	}
+	
+//	public  RewardValue(cashValue) {
+//		
+//	}
+	
+	public double getMilesValue() {
+		return cashValue/0.0035;// cash/rate
+	}
+	public double getCashValue() {
+		return cashValue; // mile*rate
+	}
+	public void setCashValue(double cash) {
+		this.cashValue = cash;
+	}
+	}
